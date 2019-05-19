bfb35b7ac149d3a18863fc3cadb2bc8324a7e670
Renato Colaco <renatocolaco@gmail.com>
2019-05-18 21:30:01 -0300

    Add project files.

---
+++ NewTask/NewTask.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using System.Timers;
+using RabbitMQ.Client;
+
+namespace Demo2
+{
+    class NewTask
+    {
+        private static Timer messageTimer = null;
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            setTimer();
+
+            Console.ReadKey();
+        }
+
+        private static void setTimer()
+        {
+            // Create a timer with a two second interval.
+            messageTimer = new Timer(2000);
+            // Hook up the Elapsed event for the timer. 
+            messageTimer.Elapsed += OnTimedEvent;
+            messageTimer.AutoReset = true;
+            messageTimer.Enabled = true;
+        }
+
+        private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.15.33",
+                UserName = "renatocolaco",
+                Password = "secnet123"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(
+                    queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+                var message = GetMessage();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true;
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "task_queue",
+                                     basicProperties: properties,
+                                     body: body);
+            }
+
+            Console.WriteLine("A new message published to the queue at {0:HH:mm:ss.fff}",
+                              e.SignalTime);
+        }
+
+        private static string GetMessage()
+        {
+            return $"Hello World! {DateTime.Now:HH:mm:ss}";
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,61 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Worker
+{
+    class Worker
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.15.33",
+                UserName = "renatocolaco",
+                Password = "secnet123"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // This tells RabbitMQ not to give more than one message to a worker at a time
+                // prefetchCount: 1
+                channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                channel.QueueDeclare(
+                    queue: "task_queue",
+                    durable: true,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                    );
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] message received {0}", message);
+                                        
+                    Thread.Sleep(4000);
+
+                    Console.WriteLine(" [#] Done");
+
+                    // manual ack
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+
+                channel.BasicConsume(
+                    queue: "task_queue",
+                    autoAck: false, // manual ack in order to not lost messages if consumer dies
+                    consumer: consumer
+                    );
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
